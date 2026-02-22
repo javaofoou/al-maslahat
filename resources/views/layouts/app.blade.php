14bab748a0962d772edb70394f0752744dd56074
javaofoou <almaslahatilisilamiyat@gmail.com>
2026-02-22 02:13:16 -0800

    Remove Vite and switch to Tailwind CDN

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Al Maslaha</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
+<script>
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#064e3b',
+          secondary: '#84cc16'
+        }
+      }
+    }
+  }
+</script>
 </head>
 <body class="bg-white text-gray-800">
 
